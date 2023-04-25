77ff33544c6b0f8820a1c68e793ac1869cf6234d
Ritesh <riteshkumarnandy@gmail.com>
2023-04-25 10:56:23 -0400

    changing color in bars

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -5,7 +5,7 @@ const Bar:FunctionComponent<{
     data:ISkill
 }> = ({ data: {Icon,level,name} }) => {
   return (
-    <div className='my-2 text-white rounded-full bg-slate-400'>
+    <div className='my-2 text-white rounded-full bg-slate-300'>
         <div className="flex items-center px-4 py-1 rounded-full bg-gradient-to-r from-orange-500 to-yellow-500"
         style={{ width: level}}>
             <Icon className='mr-3 '/>
